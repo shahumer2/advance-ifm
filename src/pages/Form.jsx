dd9214b77b26ba0f35364273ef0b761ce5bf2d80
Aamir Nazir Kuchay <aamir1kuchay@gmail.com>
2024-07-11 09:43:07 +0530

    form

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,36 +1,42 @@
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { FaPlus, FaLinkedin, FaStreetView, FaRegAddressCard, FaSuitcase } from "react-icons/fa";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  FaPlus,
+  FaLinkedin,
+  FaStreetView,
+  FaRegAddressCard,
+  FaSuitcase,
+} from "react-icons/fa";
 import { IoIosPhonePortrait } from "react-icons/io";
 import { MdOutlineMail } from "react-icons/md";
 import { TbDeviceLandlinePhone, TbWorldLatitude } from "react-icons/tb";
 import { IoFileTrayFullSharp } from "react-icons/io5";
 import { TiWorld } from "react-icons/ti";
 import { AiFillProfile } from "react-icons/ai";
-import { ADD_EMP } from '../constants/utils';
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { useSelector } from 'react-redux';
-import './CardComponent.css'
+import { ADD_EMP } from "../constants/utils";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useSelector } from "react-redux";
+import "./CardComponent.css";
 function FormComponent() {
   const { currentUser } = useSelector((state) => state?.persisted?.user);
   const { token } = currentUser;
-  const navigate=useNavigate();
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({
-    firstName: '',
-    lastName: '',
-    mobileNumber: '',
-    profile: '',
-    email: '',
-    linkedInUrl: '',
-    alternateNumber: '',
-    companyName: '',
-    country: '',
-    state: '',
-    street: '',
-    pincode: '',
-    profession: '',
-    photo: null // Initialize photo as null
+    firstName: "",
+    lastName: "",
+    mobileNumber: "",
+    profile: "",
+    email: "",
+    linkedInUrl: "",
+    alternateNumber: "",
+    companyName: "",
+    country: "",
+    state: "",
+    street: "",
+    pincode: "",
+    profession: "",
+    photo: null, // Initialize photo as null
   });
 
   const handleChange = (e) => {
@@ -45,7 +51,7 @@ function FormComponent() {
     // Preview photo (if needed)
     const reader = new FileReader();
     reader.onload = () => {
-      document.getElementById('photoPreview').src = reader.result;
+      document.getElementById("photoPreview").src = reader.result;
     };
     reader.readAsDataURL(file);
   };
@@ -60,55 +66,83 @@ function FormComponent() {
 
       const response = await fetch(ADD_EMP, {
         method: "POST",
-        headers:{
-          "Authorization":`Bearer ${token}`
+        headers: {
+          Authorization: `Bearer ${token}`,
         },
-        body: formDataToSend
+        body: formDataToSend,
       });
-      
+
       const data = await response.json();
       toast.success(" Employee Added Successfully !");
       // console.log(data, "Response from server");
-     
-     
 
-      navigate("/profile/view")
+      navigate("/profile/view");
     } catch (error) {
       console.log(error);
     }
   };
 
   return (
-    <section className="sec" style={{ padding: '2rem' }}>
+    <section className="sec" style={{ padding: "2rem" }}>
       <div className="card mr-3 ml-3 w-full h-full">
         <div className="card-body bg-white">
           <h2 className="font-weight-bold text-center text-uppercase mb-1 font-custom">
-           Add Employee
+            Add Employee
           </h2>
-         
-          <hr style={{ height: '1px', borderWidth: '0', color: 'black', backgroundColor: 'black' }} />
+
+          <hr
+            style={{
+              height: "1px",
+              borderWidth: "0",
+              color: "black",
+              backgroundColor: "black",
+            }}
+          />
           <div className="container-fluid mt-1">
-            <form onSubmit={handleSubmit} className="mt-2" encType="multipart/form-data">
-            <div className="row" style={{marginBottom:"40px"}}>
+            <form
+              onSubmit={handleSubmit}
+              className="mt-2"
+              encType="multipart/form-data"
+            >
+              <div className="row" style={{ marginBottom: "40px" }}>
                 <div className="col-md-12">
                   <div className="row">
                     <div className="col-md-6 text-center pt-5 mb-3">
-                      <label htmlFor="profilephoto" className="btn btn-outline-danger">Upload photo</label>
+                      <label
+                        htmlFor="profilephoto"
+                        className="btn btn-outline-danger"
+                      >
+                        Upload photo
+                      </label>
                       <input
                         type="file"
                         name="photo"
                         id="profilephoto"
                         className="form-control profilephoto"
                         accept="image/jpeg, image/png, image/jpg"
-                        style={{ height: '2.8rem', display: 'none' }}
+                        style={{ height: "2.8rem", display: "none" }}
                         required
                         onChange={handleFileChange}
                       />
                     </div>
                     <div className="col-md-6 ml-8">
                       <div className="col-md-4 offset-md-4 ml-9">
-                        <div style={{ border: '2px solid black ml-5', height: '150px', width: '150px', borderRadius: '4px', overflow: 'hidden' }}>
-                          <img alt="photoPreview" id="photoPreview" src="" height="150px" width="150px" />
+                        <div
+                          style={{
+                            border: "2px solid black ml-5",
+                            height: "150px",
+                            width: "150px",
+                            borderRadius: "4px",
+                            overflow: "hidden",
+                          }}
+                        >
+                          <img
+                            alt="photoPreview"
+                            id="photoPreview"
+                            src=""
+                            height="150px"
+                            width="150px"
+                          />
                         </div>
                       </div>
                     </div>
@@ -265,11 +299,21 @@ function FormComponent() {
                       onChange={handleChange}
                     >
                       <option value="">Choose One...</option>
-                      <option value="Advancer IFM Services">Advancer IFM Services</option>
-                      <option value="Cleaning & Stewarding">Cleaning & Stewarding</option>
-                      <option value="Pest Control & Fumigation">Pest Control & Fumigation</option>
-                      <option value="Landscape & Gardening">Landscape & Gardening</option>
-                      <option value="Security & Technology">Security & Technology</option>
+                      <option value="Advancer IFM Services">
+                        Advancer IFM Services
+                      </option>
+                      <option value="Cleaning & Stewarding">
+                        Cleaning & Stewarding
+                      </option>
+                      <option value="Pest Control & Fumigation">
+                        Pest Control & Fumigation
+                      </option>
+                      <option value="Landscape & Gardening">
+                        Landscape & Gardening
+                      </option>
+                      <option value="Security & Technology">
+                        Security & Technology
+                      </option>
                     </select>
                     <div className="input-group-append">
                       <span className="input-group-text">Department Name</span>
@@ -373,15 +417,27 @@ function FormComponent() {
               </div>
               <div className="row">
                 <div className="col-md-12 mt-3 info">
-                 
-                    <p><strong>Note:*</strong></p>
-                    <p><small> Upload Image Less Than 2 Mb.</small></p>
-                    <p><small> Only JPG,JPEG,PNG Files Are Allowed </small></p>
-                    <p><small> All Fields Are Mandatory </small></p>
-                
+                  <strong>Note:</strong>
+
+                  <span style={{ color: "red" }}>*</span>
+                  <small> Upload Image Less Than 2 Mb.</small>
+
+                  <span style={{ color: "red" }}>*</span>
+                  <small> Only JPG,JPEG,PNG Files Are Allowed </small>
+
+                  <p>
+                    <span style={{ color: "red" }}>*</span>
+                    <small> All Fields Are Mandatory </small>
+                  </p>
                 </div>
                 <div className="container text-center mt-3">
-                  <button className="btn btn-outline-primary" type="submit">Save</button>
+                  <button
+                    style={{ width: "150px" }}
+                    className="btn btn-outline-primary custom-button"
+                    type="submit"
+                  >
+                    Save
+                  </button>
                 </div>
               </div>
             </form>
